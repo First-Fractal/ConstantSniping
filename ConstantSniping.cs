e31dfd3881ded0642e6da7a4e78cbc1a91c3d0f8
First Fractal <firstfractal512@gmail.com>
2023-02-21 16:58:45 -0600

    made the crosshair randomly spawn in and chase the player

--- ConstantSniping.cs
+++ ConstantSniping.cs
@@ -1,8 +1,66 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.Chat;
 using Terraria.ModLoader;
+using Terraria.Localization;
+using Microsoft.Xna.Framework;
+using ConstantSniping.Projectiles;
 
 namespace ConstantSniping
 {
 	public class ConstantSniping : Mod
 	{
-	}
+        public void Talk(string message, Color color)
+        {
+            if (Main.netMode == NetmodeID.SinglePlayer)
+            {
+                Main.NewText(message, color);
+            }
+            else
+            {
+                ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral(message), color);
+            }
+        }
+    }
+    public class ConstantSnipingSystem : ModSystem
+    {
+        int counter = 0;
+        public int spawnerCounter = 0;
+        ConstantSniping CS = new ConstantSniping();
+        public void bomberPlaneSpawner()
+        {
+            counter++;
+            if (counter >= 60)
+            {
+                spawnerCounter++;
+                counter = 0;
+                CS.Talk(spawnerCounter.ToString(), Color.LightSeaGreen);
+            }
+
+            if (spawnerCounter >= 5)
+            {
+                CS.Talk("Target Sighted", Color.OrangeRed);
+
+                Player target = null;
+                while (true) {
+                    target = Main.player[Main.rand.Next(0, Main.player.Length)];
+                    if (target.active)
+                    {
+                        break;
+                    }
+                }
+
+                CS.Talk(target.position.ToString(), Color.OliveDrab);
+                Projectile.NewProjectile(target.GetSource_FromThis(), target.position, Vector2.Zero, ModContent.ProjectileType<Crosshair>(), 0, 0);
+
+                spawnerCounter = 0;
+            }
+        }
+
+        public override void PostUpdateEverything()
+        {
+            bomberPlaneSpawner();
+            base.PostUpdateEverything();
+        }
+    }
 }
--- Projectiles/Crosshair.cs
+++ Projectiles/Crosshair.cs
@@ -1,5 +1,6 @@
 ﻿using Terraria;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
 
 namespace ConstantSniping.Projectiles
 {
@@ -7,7 +8,74 @@ namespace ConstantSniping.Projectiles
     {
         public override void SetDefaults()
         {
+            Projectile.width = 90;
+            Projectile.height = 90;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = 800;
+            Projectile.penetrate = -1;
+            Projectile.aiStyle = -1;
+            Projectile.alpha = 255;
+
             base.SetDefaults();
+        }
+
+        
+
+        public override void AI()
+        {
+
+            if (Projectile.alpha > 0)
+            {
+                Projectile.alpha-=3;
+            }
+
+            Player target = null;
+            foreach (Player player in Main.player)
+            {
+                if (target == null)
+                {
+                    target = player;
+                }
+                else
+                {
+                    float tarDis = Vector2.Distance(Projectile.position, target.position);
+                    float playerDis = Vector2.Distance(Projectile.position, player.position);
+
+                    if (playerDis < tarDis)
+                    {
+                        target = player;
+                    }
+                }
+            }
+
+
+            Vector2 dir = new Vector2(target.position.X - Projectile.position.X, target.position.Y - Projectile.position.Y) + new Vector2(-35, -18);
+            dir = Vector2.Normalize(dir);
+
+            Projectile.position += dir * 3;
+
+            base.AI();
+        }
+
+        public override bool? CanHitNPC(NPC target)
+        {
+            return false;
+        }
+
+        public override bool CanHitPlayer(Player target)
+        {
+            return false;
+        }
+
+        public override bool CanHitPvp(Player target)
+        {
+            return false;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Main.NewText("insert a gunshot here");
+            base.Kill(timeLeft);
         }
     }
 }
